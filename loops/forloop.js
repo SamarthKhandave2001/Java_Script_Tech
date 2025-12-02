31d43d2dd65d5f74220dd813bb416b367435a960
SamarthKhandave2001 <khandavesamarth@gmail.com>
2025-12-02 12:50:15 +0530

    pratice 5 table

--- loops/forloop.js
+++ loops/forloop.js
@@ -4,3 +4,10 @@ for (var num =1; num<=10; num ++)
 {
     console.log(num);
 }
+//pratice 
+//create a table of 5
+var number =1;
+while(number <=10){
+    console.log('5 *' + number + ' = ' + 5 * number);
+    number ++
+}
