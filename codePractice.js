0fbd04dab29d25ccc332e79be063ea1668ccea61
SamarthKhandave2001 <khandavesamarth@gmail.com>
2025-12-03 15:20:48 +0530

    prime number added

--- codePractice.js
+++ codePractice.js
@@ -19,4 +19,34 @@ if(num===0){
 
 }
 
-// Switch statement 
+
+
+//calaulate the sum of number from 1 to 10 using lopp 
+var sum =0;
+for (var num =1; num <=10; num ++){
+ var sum = sum + num;
+ 
+}
+console.log(sum);
+
+//program to check number is prime or not
+
+var number = 12;
+var isPrime = true;
+for(var i=2;  i<number; i++){
+    if(number % i===0){
+        isPrime = false;
+        break;
+    }
+}
+if(isPrime){
+    console.log(number + "Num is Prime");
+}else{
+    console.log(number +"Num is not Prime");
+}
+
+//Generate a time table with the for loop
+for(var num =1; num<=10; num ++){
+
+console.log("5 * " + num + " =" + 5 * num);
+}
--- loops/doWhileLoop.js
+++ loops/doWhileLoop.js
@@ -4,3 +4,16 @@ do{
     i++;                    
 }
 while(i <= 10);
+
+
+//new practice
+// example : validating  user input with a do while loop (user need to write a valid number )
+
+let userInput;
+let positiveNumber;
+
+do {
+    userInput = prompt('Enter any positive number:');
+    positiveNumber = Number(userInput);
+} while (isNaN('positiveNumber') || positiveNumber < 0);
+console.log('You enter a valid positive Number: ' + positiveNumber);
