d7c658d403a81a9f059f59e2420d6c87c2141dc0
SamarthKhandave2001 <khandavesamarth@gmail.com>
2025-12-04 12:13:09 +0530

    function code

---
+++ Function/function.js
@@ -0,0 +1,10 @@
+//3 student at a same time wnats to find  the sum of two numbers
+
+var a = 5, b = 10;
+var total = a + b;
+console.log(total);
+
+function add(a, b) {
+    return a + b;
+}
+console.log(add(5, 10))
