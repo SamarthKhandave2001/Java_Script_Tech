32b96ed9644d7ae3f258d3f29b1043b00a3dacf6
SamarthKhandave2001 <khandavesamarth@gmail.com>
2025-12-04 16:14:30 +0530

    small

--- Function/question.js
+++ Function/question.js
@@ -1,6 +1,14 @@
-//string interpolation : Template strings  support string interpolation, allowing ypu to embed expression directly within the stirnf, Interpolated expression are inclosed in $()
+//string interpolation : Template strings  support string interpolation, allowing ypu to embed expression directly within the stirng, Interpolated expression are inclosed in $()
 
 
 let age =29;
 let message =`My age is ${age} years`;  // this is known as string interpolation
 console.log(message);
+
+//write function sum of two numbers
+
+
+function sum(a,b){
+    return  a + b;
+}
+console.log(sum(5,15));
