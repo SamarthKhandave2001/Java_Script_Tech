98df08dc0b48499271869c673757fc00ae1991d2
SamarthKhandave2001 <khandavesamarth@gmail.com>
2025-12-04 16:01:34 +0530

    string interpolation

---
+++ Function/question.js
@@ -0,0 +1,6 @@
+//string interpolation : Template strings  support string interpolation, allowing ypu to embed expression directly within the stirnf, Interpolated expression are inclosed in $()
+
+
+let age =29;
+let message =`My age is ${age} years`;  // this is known as string interpolation
+console.log(message);
