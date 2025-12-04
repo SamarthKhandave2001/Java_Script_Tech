3d5d2aa74f0f483b6911741f120fd6d5148ae7ae
SamarthKhandave2001 <khandavesamarth@gmail.com>
2025-12-04 18:03:15 +0530

    theory

--- Function/question.js
+++ Function/question.js
@@ -35,7 +35,8 @@ function calculator(num1, num2, operator) {
 
 console.log(calculator(10,5,"+"));
 
-//new 
+//Map Function 
+//Function expressions are convenient when passing a function as an argument to another function. The following example defines a map function that should receive a function as first argument and an array as second argument. Then, it is called with a function defined by a function expression:
 
 function map(f, a) {
   const result = new Array(a.length);
