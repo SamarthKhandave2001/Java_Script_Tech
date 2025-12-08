c0bda3573eca0b423e646c047bc89cd4e34bee30
SamarthKhandave2001 <khandavesamarth@gmail.com>
2025-12-08 16:21:04 +0530

    indexing

--- Array/firstArray.js
+++ Array/firstArray.js
@@ -9,3 +9,8 @@ console.log(fruits);
 
 //using array literals 
 let newFruits = ["Apple","Orange","Mango"];
+
+//Access elements of an array using index
+
+let firstFruit = ["Apple","Orange","Mango"];
+console.log(firstFruit[0]); // Apple
