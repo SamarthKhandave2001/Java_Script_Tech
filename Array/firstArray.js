42f6a629695a37d1edc1e3a584462ad26434f3f4
SamarthKhandave2001 <khandavesamarth@gmail.com>
2025-12-08 16:18:10 +0530

    arrayLiterals

--- Array/firstArray.js
+++ Array/firstArray.js
@@ -6,3 +6,6 @@ console.log(arr); //
 // using Array constructor 
 let fruits = new Array("Apple","Orange","Mango");
 console.log(fruits);
+
+//using array literals 
+let newFruits = ["Apple","Orange","Mango"];
