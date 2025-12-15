d2d76ca808118968455e2f65f76263e2dbedd90e
SamarthKhandave2001 <khandavesamarth@gmail.com>
2025-12-15 18:29:45 +0530

    vowels

--- Array/question.js
+++ Array/question.js
@@ -93,3 +93,22 @@ for (let i = 0; i < arr6.length; i++) {
 }
 
 console.log(freq);
+
+//Count vowels in a string
+let str = "javascript";
+let count = 0;
+
+for (let i = 0; i < str.length; i++) {
+  let ch = str[i];
+
+  if (
+    ch === 'a' || ch === 'e' || ch === 'i' ||
+    ch === 'o' || ch === 'u' ||
+    ch === 'A' || ch === 'E' || ch === 'I' ||
+    ch === 'O' || ch === 'U'
+  ) {
+    count++;
+  }
+}
+
+console.log(count); // 3
