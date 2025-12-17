b39bffd3c47df79b4ecd582a19cd4ee900b275d0
SamarthKhandave2001 <khandavesamarth@gmail.com>
2025-12-17 12:36:15 +0530

    new Example

--- String/stringExercise.js
+++ String/stringExercise.js
@@ -2,3 +2,7 @@
 const str = "Hello Welcome to JavaScript";
 console.log(str.length); //length of string
 // length always start with 1
+
+//another example
+let text = 'My name is "Sam" & I am a Full-Stack Developer';
+console.log(text);
