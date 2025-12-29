44916617df2f6917202bba373c9c43195bafc9a7
SamarthKhandave2001 <khandavesamarth@gmail.com>
2025-12-29 18:20:01 +0530

    secondlast element

--- Array/question.js
+++ Array/question.js
@@ -142,3 +142,8 @@ for (let i = 0; i < arr9.length; i++) {
 }
 
 console.log(result); // [1,2,3]
+
+let arr10 = [1, 2, 3, 4, 5];
+let secondLast = arr10[arr10.length - 2];
+
+console.log(secondLast); // 4
