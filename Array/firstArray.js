c3585960ccc427459ce8e2638fac5dee669cf099
SamarthKhandave2001 <khandavesamarth@gmail.com>
2026-01-07 12:55:45 +0530

    comment

--- Array/firstArray.js
+++ Array/firstArray.js
@@ -158,7 +158,7 @@ const result7 = nums.map((curElem) => {
 console.log(result7);
 
 //Using the map method, write a function that takes an array of names and returns new array where each name is prefixed with "Mr."
-
+//
 const personNames = ["John","David","Sam","Michael"];
 const prefixName = personNames.map((curElem) => `Mr. ${curElem}`);
 console.log(prefixName); // ["Mr. John","Mr. David","Mr. Sam","Mr. Michael"]
